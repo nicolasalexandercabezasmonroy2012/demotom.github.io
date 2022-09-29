3d2d1cec7f66f87346ac333d6693930fafc6c243
nicolasalexandercabezasmonroy2012 <114183851+nicolasalexandercabezasmonroy2012@users.noreply.github.com>
2022-09-29 13:40:07 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function backgroundchangeimages(fondo) {
+    document.body.style.backgroundImage = `url(${fondo})`
+    document.body.style.backgroundRepeat = 'no-repeat'
+    document.body.style.backgroundSize = 'cover'
+  } 
